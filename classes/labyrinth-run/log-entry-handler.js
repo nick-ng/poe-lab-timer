9e2056218b6fa772f50f814e8840f57ad5b3f158
Nick Ng <nicholas.cj.ng@gmail.com>
2017-09-17 13:29:16 +1200

    Started making separate log entry handler file.

---
+++ classes/labyrinth-run/log-entry-handler.js
@@ -0,0 +1,16 @@
+const logEntryHandler = (currentState, nextState, logEntry) => {
+  /*
+  if (currentState === 'ready') {
+    // if log entry is an izaro line
+    // start timing
+  }
+  switch (nextState) {
+    case 'ready':
+      // do a thing
+      break;
+    case 'izaro':
+      // do a thing
+      break;
+    case '
+  */
+};
